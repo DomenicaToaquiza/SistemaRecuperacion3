b9ba77b86ee30167d42b63723fa1fa1552fb721d
DomenicaToaquiza <154259914+DomenicaToaquiza@users.noreply.github.com>
2025-06-05 13:19:53 -0500

    Creacion de models

--- Aplicaciones/SistemaMonumento/models.py
+++ Aplicaciones/SistemaMonumento/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Monumento(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True, null=True)
+
+class VisitaGuiada(models.Model):
+    monumento = models.ForeignKey(Monumento, on_delete=models.CASCADE, related_name='visitas')
+    fecha = models.DateField()
+    guia = models.CharField(max_length=100)
+    duracion_minutos = models.PositiveIntegerField()
+    observaciones = models.TextField(blank=True, null=True)
