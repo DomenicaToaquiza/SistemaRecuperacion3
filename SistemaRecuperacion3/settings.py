51d8fbd1d65ee8ab46d9bb9a417c322c6a30aecf
DomenicaToaquiza <154259914+DomenicaToaquiza@users.noreply.github.com>
2025-06-05 15:25:39 -0500

    Implementacion de la plantilla

--- SistemaRecuperacion3/settings.py
+++ SistemaRecuperacion3/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'SistemaMonumento/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -125,3 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Configurando el acceso a la carpeta media
 MEDIA_URL='/media/'
 MEDIA_ROOT=(os.path.join(BASE_DIR,'SistemaRecuperacion3/media/'))
+
